af4bff5a40499fa1a16214ae1b11e77a076d3e73
Duc Huynh <duchuynh1797@gmail.com>
2018-12-07 18:47:51 +0700

    2so

---
+++ vidu0/2so.js
@@ -0,0 +1,18 @@
+var a = 4;
+var b = 5;
+function plus(a , b){
+    document.write("tong la "+ (a+b)+"<br>");
+}
+console.log(plus(a,b));
+function minus(a , b){
+    document.writeln("hieu la "+ (a-b)+"<br>");
+}
+console.log(minus(a,b));
+function multiplication(a , b){
+    document.writeln("tich la "+ (a*b)+"<br>");
+}
+console.log(multiplication(a,b));
+function division(a , b){
+    document.writeln("thuong la "+ (a/b));
+}
+console.log(division(a,b));
